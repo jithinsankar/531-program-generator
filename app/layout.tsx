91b73c61587255872726d53e8d2d6d5e94e34ffe
jithinsankar <jithinsankar.nk@gmail.com>
2024-10-19 13:25:37 +0530

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🏋🏽</text></svg>"
+      />
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
