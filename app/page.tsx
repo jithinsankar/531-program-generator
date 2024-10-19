639d04000f14639a6798b1f72639d863ed505840
jithinsankar <jithinsankar.nk@gmail.com>
2024-10-19 12:42:36 +0530

    added GH link

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://github.com/jithinsankar"
+          href="https://github.com/jithinsankar/531-program-generator"
           target="_blank"
           rel="noopener noreferrer"
         >
